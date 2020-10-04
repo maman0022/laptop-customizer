b8c1b502ea23c1a3dbdf8449baebc88ec53ad88f
Muhammad Aman <maman0022@outlook.com>
2020-10-03 20:32:53 -0500

    built the app into static version

---
+++ docs/precache-manifest.986edfbfad04f22cdc816c1acc981396.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85d1fc445415725816aff2775b80b85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8392bbfd8e4423ae0040",
+    "url": "/static/css/main.606b96d0.chunk.css"
+  },
+  {
+    "revision": "91be17f00bf027269ee1",
+    "url": "/static/js/2.a3f18fbf.chunk.js"
+  },
+  {
+    "revision": "8392bbfd8e4423ae0040",
+    "url": "/static/js/main.21e35029.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
